a96e5e7bafbe6c78e463c7652a57ba8ded74de4c
LashikaBhartiya <104684875+LashikaBhartiya@users.noreply.github.com>
2024-06-09 22:59:26 +0530

    quest1

---
+++ quest1.py
@@ -0,0 +1,13 @@
+# Program to take two numbers as input and print their sum
+
+# Taking first number as input from the user
+number1 = float(input("Enter the first number: "))
+
+# Taking second number as input from the user
+number2 = float(input("Enter the second number: "))
+
+# Calculating the sum of the two numbers
+sum_result = number1 + number2
+
+# Printing the sum
+print("The sum of", number1, "and", number2, "is:", sum_result)
